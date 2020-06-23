ee4863742bcad62d75c1934b82c684405c6e155a
Kelleymb <kelleybuccieri@gmail.com>
2020-06-23 10:01:08 -0400

    initial commit

---
+++ checkpt18_assignment.sql
@@ -0,0 +1,12 @@
+-- 1. How many people work in the Sales department?
+
+-- 2. List the names of all employees assigned 
+--    to the 'Plan Christmas party' project.
+
+-- 3. List the names of employees from the Warehouse 
+--    department that are assigned to the 'Watch paint dry' project.
+
+-- 4. Which projects are the Sales department employees assigned to?
+
+-- 5. List only the managers that are assigned to 
+--    the 'Watch paint dry' project.
